7d26a09d1fa85a02f1fcca5a704dd323cfd82419
Maximilian <maximilian@ernestus.de>
2013-01-07 17:45:04 +0100

    Added documentation for the virtual block memory

--- wiselib.testing/external_interface/pc/pc_virtual_blockmemory.h
+++ wiselib.testing/external_interface/pc/pc_virtual_blockmemory.h
@@ -11,21 +11,36 @@
 
 namespace wiselib {
 
+/*
+* @brief A virtual bock memory for the PC os model. It implements the block memory interface.
+* @author Maximilian Ernestus
+*
+* The data is stored in the ram (huge waste but who cares, it is just used for debugging)
+* It was written to debug programs, that
+* @tparam OsModel_P specifies the os model to use (pc os model in this case)
+* @tparam nrOfBlocks specifies the number of blocks we want to use (default 1000)
+* @tparam blocksize the size of the blocks you want to use (deault 512)
+*/
 template<typename OsModel_P, int nrOfBlocks = 1000, int blocksize = 512>
 class VirtualSD {
 
 public:
+	/*
+	 * weird wiselib stuff comes here
+	 */
 	typedef OsModel_P OsModel;
 	typedef typename OsModel::block_data_t block_data_t;
 	typedef typename OsModel::size_t size_type;
 	typedef size_type address_t; /// always refers to a block number
 	typedef VirtualSD self_type;
 	typedef self_type* self_pointer_t;
-
 	enum {
 		SUCCESS = OsModel::SUCCESS, ERR_UNSPEC = OsModel::ERR_UNSPEC
 	};
 
+	/*
+	 * Generates
+	 */
 	VirtualSD() {
 		for (int i = 0; i < nrOfBlocks; i++) {
 			isWritten[i] = false;
@@ -33,11 +48,19 @@ public:
 		resetStats();
 	}
 
+	/*
+	 * Not implemented yet! Who wants this anyways?
+	 */
 	int erase(address_t start_block, address_t blocks) {
 		//TODO
 		return SUCCESS;
 	}
 
+	/*
+	 * Writes some data to a block.
+	 * @param x An array of data to be written. The array is assumed to be of size *blocksize*.
+	 * @param block the number of the block to write into.
+	 */
 	int write(block_data_t* x, address_t block) {
 		++ios_;
 		++blocksWritten_;
@@ -54,122 +77,163 @@ public:
 		return SUCCESS;
 	}
 
-int write(block_data_t* x, address_t start_block, address_t blocks) {
-	++ios_;
-	duration_ += 4;
+	/*
+	 * Writes data from a big array into multiple sucessive blocks.
+	 * @param x The array of data to be written. The array is assumed to be of size blocks*blocksize.
+	 * @start_block The number of the block where to start writing the data.
+	 * @blocks The numer of blocks to write into.
+	 */
+	int write(block_data_t* x, address_t start_block, address_t blocks) {
+		++ios_;
+		duration_ += 4;
 
-	for (int i = 0; i < blocks; i++) {
-		write(start_block + i, x + blocksize * i);
-		duration_ -= 6;
-		--ios_;
+		for (int i = 0; i < blocks; i++) {
+			write(start_block + i, x + blocksize * i);
+			duration_ -= 6;
+			--ios_;
+		}
+		return SUCCESS;
 	}
-	return SUCCESS;
-}
 
-int read(block_data_t* buffer, address_t block) {
-	if (block < 0 || block >= nrOfBlocks) {
-		std::cerr << "OVERFLOW VIRTUAL SD" << std::endl;
-		return ERR_UNSPEC;
+	/*
+	 * Reads some data from a block and stores it into a buffer.
+	 * @param buffer a pointer to a buffer to place the data into.
+	 * @param block the block to read from.
+	 */
+	int read(block_data_t* buffer, address_t block) {
+		if (block < 0 || block >= nrOfBlocks) {
+			std::cerr << "OVERFLOW VIRTUAL SD" << std::endl;
+			return ERR_UNSPEC;
+		}
+		++ios_;
+		++blocksRead_;
+		duration_ += 4;
+		//else if(!isWritten[block]) std::cerr << "READING EMPTY BLOCK" << std::endl;
+
+		for (int i = 0; i < blocksize; i++)
+			buffer[i] = memory[block][i];
+		return SUCCESS;
 	}
-	++ios_;
-	++blocksRead_;
-	duration_ += 4;
-	//else if(!isWritten[block]) std::cerr << "READING EMPTY BLOCK" << std::endl;
 
-	for (int i = 0; i < blocksize; i++)
-		buffer[i] = memory[block][i];
-	return SUCCESS;
-}
+	/*
+	 * Reads data from multiple sucessive blocks at once and stores it into a buffer.
+	 * @param buffer The buffer to store the data into. It is assumed to be of lenth length*blocksize
+	 * @param block The number of the block where to start reading from.
+	 * @param length the number of blocks to read.
+	 */
+	int read(block_data_t* buffer, address_t block, address_t length) {
+		++ios_;
+		duration_ += 2;
 
-int read(block_data_t* buffer, address_t block, address_t length) {
-	++ios_;
-	duration_ += 2;
-
-	for (int i = 0; i < length; i++) {
-		read(block + i, buffer + blocksize * i);
-		duration_ -= 2;
-		--ios_;
+		for (int i = 0; i < length; i++) {
+			read(block + i, buffer + blocksize * i);
+			duration_ -= 2;
+			--ios_;
+		}
+		return SUCCESS;
 	}
-	return SUCCESS;
-}
 
-void resetStats() {
-	blocksWritten_ = 0;
-	blocksRead_ = 0;
-	ios_ = 0;
-	duration_ = 0;
-}
+	/*
+	 * Resets all the counters that keep track of the IO's
+	 */
+	void resetStats() {
+		blocksWritten_ = 0;
+		blocksRead_ = 0;
+		ios_ = 0;
+		duration_ = 0;
+	}
 
-void printStats() {
-	std::cout << "Blocks Written: " << blocksWritten_ << std::endl;
-	std::cout << "Blocks Read: " << blocksRead_ << std::endl;
-	std::cout << "IOs: " << ios_ << std::endl;
-	std::cout << "Duration: " << duration_ << std::endl;
-	std::cout << "AvgIO: " << duration_ / ios_ << std::endl;
-}
+	/*
+	 * Prints usage statistics about the virtual sd card to the console.
+	 */
+	void printStats() {
+		std::cout << "Blocks Written: " << blocksWritten_ << std::endl;
+		std::cout << "Blocks Read: " << blocksRead_ << std::endl;
+		std::cout << "IOs: " << ios_ << std::endl;
+		std::cout << "Duration: " << duration_ << std::endl;
+		std::cout << "AvgIO: " << duration_ / ios_ << std::endl;
+	}
 
-void printGraphBytes(int fromBlock, int toBlock)
-{
-	if(fromBlock < 0 || toBlock > nrOfBlocks) return;
-
-	std::cout << "digraph BM {" << std::endl << "\t node [shape=none, margin=0];" << std::endl;
-	std::cout << "\t sdcard" << " [label=<";
-	printHTMLTableBytes(fromBlock, toBlock);
-	std::cout <<">];" << std::endl;
-	std::cout << "}" << std::endl;;
-}
-
-void printGraphBlocks(int fromBlock, int toBlock)
-{
-	if(fromBlock < 0 || toBlock > nrOfBlocks) return;
-
-	std::cout << "digraph BM {" << std::endl << "\t node [shape=none, margin=0];" << std::endl;
-	std::cout << "\t sdcard" << " [label=<";
-	printHTMLTableBlocks(fromBlock, toBlock);
-	std::cout <<">];" << std::endl;
-	std::cout << "}" << std::endl;;
-}
-
-void printHTMLTableBytes(int fromBlock, int toBlock)
-{
-	if(fromBlock < 0 || toBlock > nrOfBlocks) return;
-	std::cout << "<table border=\"0\" cellborder=\"1\" cellspacing=\"0\">";
-	for(int block = fromBlock; block <= toBlock; block++)
+	/*
+	 * Prints out a graphviz graph displaying every single byte.
+	 * Non-null bytes are colored red so it is easier to see what parts are used.
+	 * @param fromBlock The number of the block where to start displaying.
+	 * @param toBlock The number of the block where to stop displaying.
+	 */
+	void printGraphBytes(int fromBlock, int toBlock)
 	{
-		std::cout << "\n\t\t<tr>\n";
-		for(int j = 0; j < blocksize; j++)
+		if(fromBlock < 0 || toBlock > nrOfBlocks) return;
+
+		std::cout << "digraph BM {" << std::endl << "\t node [shape=none, margin=0];" << std::endl;
+		std::cout << "\t sdcard" << " [label=<";
+		printHTMLTableBytes(fromBlock, toBlock);
+		std::cout <<">];" << std::endl;
+		std::cout << "}" << std::endl;;
+	}
+
+	/*
+	 * Prints out a graphiz graph displaying the specified blocks.
+	 * Used blocks are marked red.
+	 * @param fromBlock The number of the block where to start displaying.
+	 * @param toBlock The number of the block where to stop displaying.
+	 */
+	void printGraphBlocks(int fromBlock, int toBlock)
+	{
+		if(fromBlock < 0 || toBlock > nrOfBlocks) return;
+
+		std::cout << "digraph BM {" << std::endl << "\t node [shape=none, margin=0];" << std::endl;
+		std::cout << "\t sdcard" << " [label=<";
+		printHTMLTableBlocks(fromBlock, toBlock);
+		std::cout <<">];" << std::endl;
+		std::cout << "}" << std::endl;;
+	}
+
+	/*
+	 * Helper for printGraphBytes
+	 */
+	void printHTMLTableBytes(int fromBlock, int toBlock)
+	{
+		if(fromBlock < 0 || toBlock > nrOfBlocks) return;
+		std::cout << "<table border=\"0\" cellborder=\"1\" cellspacing=\"0\">";
+		for(int block = fromBlock; block <= toBlock; block++)
 		{
-			std::cout << "\t\t\t<td bgcolor=\"" << ((int)memory[block][j] == 0 ? "#FFFFFF" : "#FF0000") << "\">" << (int)memory[block][j] << "</td>\n";
+			std::cout << "\n\t\t<tr>\n";
+			for(int j = 0; j < blocksize; j++)
+			{
+				std::cout << "\t\t\t<td bgcolor=\"" << ((int)memory[block][j] == 0 ? "#FFFFFF" : "#FF0000") << "\">" << (int)memory[block][j] << "</td>\n";
+				//std::cout << "\t\t\t<td bgcolor=\"" << ((int)memory[block][j] == 0 ? "#FFFFFF" : "#FF0000") << "\">" << " " << "</td>\n";
+			}
+			std::cout << "\t\t</tr>\n";
+		}
+		std::cout << "</table>";
+	}
+
+	/*
+	 * Helper for printGraphBlocks
+	 */
+	void printHTMLTableBlocks(int fromBlock, int toBlock)
+	{
+		if(fromBlock < 0 || toBlock > nrOfBlocks) return;
+		std::cout << "<table border=\"0\" cellborder=\"1\" cellspacing=\"0\">";
+		std::cout << "\n\t\t<tr>\n";
+		for(int block = fromBlock; block <= toBlock; block++)
+		{
+			std::cout << "\t\t\t<td bgcolor=\"" << (!isWritten[block] ? "#FFFFFF" : "#FF0000") << "\">" << isWritten[block] << "</td>\n";
 			//std::cout << "\t\t\t<td bgcolor=\"" << ((int)memory[block][j] == 0 ? "#FFFFFF" : "#FF0000") << "\">" << " " << "</td>\n";
+
 		}
 		std::cout << "\t\t</tr>\n";
+		std::cout << "</table>";
 	}
-	std::cout << "</table>";
-}
-
-void printHTMLTableBlocks(int fromBlock, int toBlock)
-{
-	if(fromBlock < 0 || toBlock > nrOfBlocks) return;
-	std::cout << "<table border=\"0\" cellborder=\"1\" cellspacing=\"0\">";
-	std::cout << "\n\t\t<tr>\n";
-	for(int block = fromBlock; block <= toBlock; block++)
-	{
-		std::cout << "\t\t\t<td bgcolor=\"" << (!isWritten[block] ? "#FFFFFF" : "#FF0000") << "\">" << isWritten[block] << "</td>\n";
-		//std::cout << "\t\t\t<td bgcolor=\"" << ((int)memory[block][j] == 0 ? "#FFFFFF" : "#FF0000") << "\">" << " " << "</td>\n";
-
-	}
-	std::cout << "\t\t</tr>\n";
-	std::cout << "</table>";
-}
 
 private:
-block_data_t memory[nrOfBlocks][blocksize];
-bool isWritten[nrOfBlocks];
+	block_data_t memory[nrOfBlocks][blocksize];
+	bool isWritten[nrOfBlocks];
 
-int blocksWritten_;
-int blocksRead_;
-int ios_;
-int duration_;
+	int blocksWritten_;
+	int blocksRead_;
+	int ios_;
+	int duration_;
 
 };
 } //NAMESPACE
