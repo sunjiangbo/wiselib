5501a0fa0ec8435d3467d4acec50dbbcb89aa57f
Maximilian <maximilian@ernestus.de>
2013-01-26 19:58:16 +0100

    implemented a reset and erase method.

--- wiselib.testing/external_interface/pc/pc_virtual_blockmemory.h
+++ wiselib.testing/external_interface/pc/pc_virtual_blockmemory.h
@@ -54,7 +54,12 @@ public:
 	 * Not implemented yet! Who wants this anyways?
 	 */
 	int erase(address_t start_block, address_t blocks) {
-		//TODO
+		for(int i = 0; i < blocks; i++)
+		{
+			for(int j = 0; j < blocksize; j++)
+				memory[i + start_block][j] = 0;
+		}
+
 		return SUCCESS;
 	}
 
@@ -257,6 +262,16 @@ public:
 			printf("|%s", isWritten[block] ? "1" : " ");
 		}
 		printf("|\n");
+	}
+
+	void reset()
+	{
+		for (int i = 0; i < nrOfBlocks; i++)
+			isWritten[i] = false;
+		resetStats();
+
+		erase(0, nrOfBlocks);
+
 	}
 
 private:
