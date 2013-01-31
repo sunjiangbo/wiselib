b543f7d4bb40b2878b7839a0f18cf1873f9b3675
Maximilian <maximilian@ernestus.de>
2013-01-31 08:41:22 +0100

    Just more

--- apps/generic_apps/SD_Card_HashMap/HashMapTest.cpp
+++ apps/generic_apps/SD_Card_HashMap/HashMapTest.cpp
@@ -7,6 +7,7 @@
 #include "Stopwatch.h"
 #include "HashMapIterator.h"
 #include "HashFunctionProvider.h"
+#include <stdio.h>
 
 #define NR_OF_BLOCKS_TO_TEST 8
 
@@ -29,6 +30,101 @@ public:
 		BlOCK_SIZE = 512, BLOCKS_IN_1GB = 2097152
 	};
 
+	struct Value1
+	{
+		long load;
+	};
+
+	struct Value2
+	{
+		long load1;
+		long load2;
+	};
+
+	struct Value3
+	{
+		long load1;
+		long load2;
+		long load3;
+	};
+
+	struct Value4
+	{
+		long load1;
+		long load2;
+		long load3;
+		long load4;
+	};
+
+	struct Value5
+	{
+		long load1;
+		long load2;
+		long load3;
+		long load4;
+		long load5;
+	};
+
+	struct Value6
+	{
+		long load1;
+		long load2;
+		long load3;
+		long load4;
+		long load5;
+		long load6;
+	};
+
+	struct Value7
+	{
+		long load1;
+		long load2;
+		long load3;
+		long load4;
+		long load5;
+		long load6;
+		long load7;
+	};
+
+	struct Value8
+	{
+		long load1;
+		long load2;
+		long load3;
+		long load4;
+		long load5;
+		long load6;
+		long load7;
+		long load8;
+	};
+
+	struct Value9
+	{
+		long load1;
+		long load2;
+		long load3;
+		long load4;
+		long load5;
+		long load6;
+		long load7;
+		long load8;
+		long load9;
+	};
+
+	struct Value10
+	{
+		long load1;
+		long load2;
+		long load3;
+		long load4;
+		long load5;
+		long load6;
+		long load7;
+		long load8;
+		long load9;
+		long load10;
+	};
+
 	void init(Os::AppMainParameter& value) {
 		debug_ = &wiselib::FacetProvider<Os, Os::Debug>::get_facet(value);
 		sd = &wiselib::FacetProvider<Os, Os::BlockMemory>::get_facet(value);
@@ -40,11 +136,13 @@ public:
 		sd->init();
 		//sequentialTestHashMap();
 //		loadFactorTests();
-		if(!blockTest()) debug_->debug("blockTest failed!");
-		if(!blockIteratorTest()) debug_->debug("blockIteratorTest failed!");
-		if(!hashMapTest()) debug_->debug("hashMapTest failed!");
-		if(!hashMapIteratorTest()) debug_->debug("hashMapTestIterator failed!");
-		maxLoadFactorTest();
+//		if(!blockTest()) debug_->debug("blockTest failed!");
+//		if(!blockIteratorTest()) debug_->debug("blockIteratorTest failed!");
+//		if(!hashMapTest()) debug_->debug("hashMapTest failed!");
+//		if(!hashMapIteratorTest()) debug_->debug("hashMapTestIterator failed!");
+//		maxLoadFactorTest();
+		generateGNUPLOTScripts();
+		//generateFillupAnimation();
 		exit(0);
 	}
 
@@ -546,20 +644,127 @@ public:
 
 	void maxLoadFactorTest()
 	{
-		for(int hashFunction = 0; hashFunction <=4; ++hashFunction)
+		for(int hashFunction = 0; hashFunction <=2; ++hashFunction)
 		{
-			for(int numBlocks = 1; numBlocks < 200; ++numBlocks)
+			char filename[100];
+			sprintf(filename, "hashFunction%d.dat", hashFunction);
+			FILE* file = fopen(filename, "w");
+			fprintf(file, "#numblocks loadfactor\n");
+			for(int numBlocks = 1; numBlocks < 700; ++numBlocks)
 			{
 				sd->reset();
-				wiselib::HashMap<int, long> hashMap(debug_, sd, wiselib::HashFunctionProvider<Os, int>::getHashFunction(hashFunction), 0, numBlocks);
-				long counter = 0;
+				wiselib::HashMap<long, Value1> hashMap1(debug_, sd, wiselib::HashFunctionProvider<Os, long>::getHashFunction(hashFunction), 0, numBlocks);
+				fillupHashMap<Value1>(&hashMap1);
+				float lf1 = hashMap1.getLoadFactor();
 
-				while(hashMap.putEntry(int(counter), counter) == OK)
-				{
-					counter++;
-				}
-				debug_->debug("load factor was %f with %d blocks and the hashfunction #%d", hashMap.getLoadFactor(), numBlocks, hashFunction);
+				sd->reset();
+				wiselib::HashMap<long, Value2> hashMap2(debug_, sd, wiselib::HashFunctionProvider<Os, long>::getHashFunction(hashFunction), 0, numBlocks);
+				fillupHashMap<Value2>(&hashMap2);
+				float lf2 = hashMap2.getLoadFactor();
+
+				sd->reset();
+				wiselib::HashMap<long, Value3> hashMap3(debug_, sd, wiselib::HashFunctionProvider<Os, long>::getHashFunction(hashFunction), 0, numBlocks);
+				fillupHashMap<Value3>(&hashMap3);
+				float lf3 = hashMap3.getLoadFactor();
+
+				sd->reset();
+				wiselib::HashMap<long, Value4> hashMap4(debug_, sd, wiselib::HashFunctionProvider<Os, long>::getHashFunction(hashFunction), 0, numBlocks);
+				fillupHashMap<Value4>(&hashMap4);
+				float lf4 = hashMap4.getLoadFactor();
+
+				sd->reset();
+				wiselib::HashMap<long, Value5> hashMap5(debug_, sd, wiselib::HashFunctionProvider<Os, long>::getHashFunction(hashFunction), 0, numBlocks);
+				fillupHashMap<Value5>(&hashMap5);
+				float lf5 = hashMap5.getLoadFactor();
+
+				sd->reset();
+				wiselib::HashMap<long, Value6> hashMap6(debug_, sd, wiselib::HashFunctionProvider<Os, long>::getHashFunction(hashFunction), 0, numBlocks);
+				fillupHashMap<Value6>(&hashMap6);
+				float lf6 = hashMap6.getLoadFactor();
+
+				sd->reset();
+				wiselib::HashMap<long, Value7> hashMap7(debug_, sd, wiselib::HashFunctionProvider<Os, long>::getHashFunction(hashFunction), 0, numBlocks);
+				fillupHashMap<Value7>(&hashMap7);
+				float lf7 = hashMap7.getLoadFactor();
+
+				sd->reset();
+				wiselib::HashMap<long, Value8> hashMap8(debug_, sd, wiselib::HashFunctionProvider<Os, long>::getHashFunction(hashFunction), 0, numBlocks);
+				fillupHashMap<Value8>(&hashMap8);
+				float lf8 = hashMap8.getLoadFactor();
+
+				sd->reset();
+				wiselib::HashMap<long, Value9> hashMap9(debug_, sd, wiselib::HashFunctionProvider<Os, long>::getHashFunction(hashFunction), 0, numBlocks);
+				fillupHashMap<Value9>(&hashMap9);
+				float lf9 = hashMap9.getLoadFactor();
+
+				sd->reset();
+				wiselib::HashMap<long, Value10> hashMap10(debug_, sd, wiselib::HashFunctionProvider<Os, long>::getHashFunction(hashFunction), 0, numBlocks);
+				fillupHashMap<Value10>(&hashMap10);
+				float lf10 = hashMap10.getLoadFactor();
+
+				fprintf(file, "%d %f %f %f %f %f %f %f %f %f %f\n", numBlocks, lf1, lf2, lf3, lf4, lf5, lf6, lf7, lf8, lf9, lf10);
+				debug_->debug("analyzing with %d blocks", numBlocks);
 			}
+			fclose(file);
+		}
+
+	}
+
+	void generateGNUPLOTScripts()
+	{
+		for(int hashFunction = 0; hashFunction <= 4; ++hashFunction)
+		{
+			char filename[100];
+			sprintf(filename, "comValSizes%d.dat", hashFunction);
+			FILE* file = fopen(filename, "w");
+			fprintf(file, "set term x11\n");
+			fprintf(file, "h%d = './hashFunction%d.dat'\n", hashFunction, hashFunction);
+			fprintf(file, "set style line 2  lc rgb '#0025ad' lt 1 lw 0.5 # --- blue\n");
+			fprintf(file, "set style line 3  lc rgb '#0042ad' lt 1 lw 0.5 #      .\n");
+			fprintf(file, "set style line 4  lc rgb '#0060ad' lt 1 lw 0.5 #      .\n");
+			fprintf(file, "set style line 5  lc rgb '#007cad' lt 1 lw 0.5 #      .\n");
+			fprintf(file, "set style line 6  lc rgb '#0099ad' lt 1 lw 0.5 #      .\n");
+			fprintf(file, "set style line 7  lc rgb '#00ada4' lt 1 lw 0.5 #      .\n");
+			fprintf(file, "set style line 8  lc rgb '#00ad88' lt 1 lw 0.5 #      .\n");
+			fprintf(file, "set style line 9  lc rgb '#00ad6b' lt 1 lw 0.5 #      .\n");
+			fprintf(file, "set style line 10 lc rgb '#00ad4e' lt 1 lw 0.5 #      .\n");
+			fprintf(file, "set style line 11 lc rgb '#00ad31' lt 1 lw 0.5 #      .\n");
+			fprintf(file, "set style line 12 lc rgb '#00ad14' lt 1 lw 0.5 #      .\n");
+			fprintf(file, "set style line 13 lc rgb '#09ad00' lt 1 lw 0.5 # --- green\n");
+			fprintf(file, "plot for [n=2:11] './hashFunction%d.dat' using 1:n title '(n*4)bytes' with lines ls n", hashFunction );
+			fprintf(file, "\n");
+			fclose(file);
+		}
+
+	}
+
+	template<typename ValueType>
+	void fillupHashMap(wiselib::HashMap<long, ValueType>* hashMap)
+	{
+		ValueType dummyValue;
+		long counter = 0;
+		while(hashMap->putEntry(counter, dummyValue) == OK)
+		{
+			counter++;
+		}
+
+	}
+
+	void generateFillupAnimation()
+	{
+		wiselib::HashMap<int, long> hashMap(debug_, sd, &wiselib::HashFunctionProvider<Os, int>::fnv, 0, 50);
+
+		int counter = 0;
+		long value = 0xFFFFFFFFL;
+		while(hashMap.putEntry(counter, value) == OK)
+		{
+			char filename[20];
+			sprintf(filename, "bild%d.dot", counter);
+			FILE* file = fopen(filename, "w");
+			sd->printGraphBytes(0, 100, file);
+			fclose(file);
+			debug_->debug("we inserted %d items", counter);
+			counter++;
 		}
 	}
 
@@ -588,128 +793,6 @@ public:
 			printMessage(b.getValueByID(i));
 			debug_->debug("________________");
 		}
-	}
-
-	void testBlockRemoving()
-	{
-		wiselib::Block<int, Message> b(0, sd);
-		Message m1, m2, m3;
-		m1 = makeMessage(4, 2);
-		m2 = makeMessage(99, 100);
-		m3 = makeMessage(23, 24);
-
-		b.insertValue(1, m1);
-		b.insertValue(2, m3);
-		b.insertValue(3, m2);
-
-		printBlock(b);
-		b.removeValue(2);
-		printBlock(b);
-	}
-
-	void simpleTestHashMap()
-	{
-		debug_->debug("Value with id 0:");
-		wiselib::HashMap<int, Message> hashMap(debug_, sd, &wiselib::HashFunctionProvider<Os, int>::fnv);
-		Message m1, m2, m3;
-		m1 = makeMessage(4, 2);
-		m2 = makeMessage(99, 100);
-		m3 = makeMessage(23, 24);
-
-		//First simple insertion and retrieval test
-		debug_->debug("We insert 1st entry:");
-		//hashMap.putEntry(0, m1);
-		debug_->debug("");
-		debug_->debug("We insert 2nd entry:");
-		//hashMap.putEntry(1, m2);
-
-		debug_->debug("\n");
-
-		debug_->debug("we read 1st entry:");
-		printMessage(hashMap.getEntry(0));
-		debug_->debug("");
-		debug_->debug("We read 2nd entry:");
-		printMessage(hashMap.getEntry(1));
-	}
-
-	struct SmallValue
-	{
-		int load;
-	};
-
-	struct MiddleValue
-	{
-		int load1;
-		long int load2;
-		long int load3;
-		long int load4;
-	};
-
-	struct BigValue
-	{
-		int load1;
-		long int load2;
-		long int load3;
-		long int load4;
-		long int load5;
-		long int load6;
-		long int load7;
-		long int load8;
-		long int load9;
-		long int load10;
-	};
-
-	void loadFactorTests()
-	{
-		SmallValue small;
-		MiddleValue middle;
-		BigValue big;
-
-		//debug_->debug("Test with small (%d bytes) values:", sizeof(SmallValue));
-		//debug_->debug("nr of blocks, loadfactor");
-		/*
-		for(int i = 1; i < 1000; i++)
-		{
-			wiselib::HashMap<int, SmallValue> smallValueMap(debug_, sd, 0, i);
-			int counter = 0;
-			while(smallValueMap.putEntry(counter + 99272, small))
-			{
-				counter++;
-			}
-
-			debug_->debug("%d %f", i, smallValueMap.getLoadFactor());
-			sd->erase(0, 1000);
-		}*/
-
-		/*
-		for(int i = 1; i < 1000; i++)
-		{
-			wiselib::HashMap<int, MiddleValue> middleValueMap(debug_, sd, 0, i);
-			int counter = 0;
-			while(middleValueMap.putEntry(counter + 99272, middle))
-			{
-				counter++;
-			}
-
-			debug_->debug("%d %f", i, middleValueMap.getLoadFactor());
-			sd->erase(0, 1000);
-		}*/
-
-
-		for(int i = 1; i < 1000; i++)
-		{
-			wiselib::HashMap<int, BigValue> bigValueMap(debug_, sd, 0, i);
-			int counter = 0;
-			while(bigValueMap.putEntry(counter + 99272, big) == OK)
-			{
-				counter++;
-			}
-
-			debug_->debug("%d %f", i, bigValueMap.getLoadFactor());
-			sd->erase(0, 1000);
-		}
-
-
 	}
 
 	void sequentialTestHashMap()
